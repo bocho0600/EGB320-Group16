6b742a26de94e58a56ff4f94a67f4da7692b6ee3
kelvin <nguyenleminh1002@gmail.com>
2024-08-08 17:53:36 +1000

    Adjustment on vision.py and calibrate.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import cv2
 import numpy as np
 import time
-import vision as vision
+import vision as vs
 import csv 
 import csvread as csvread
 
 def main(): # Main function
-      vision = vision.VisionModule()
+      vision = vs.VisionModule()
       cap = vision.initialize_camera()
 
       while(1):
-            print(instruction)
-
-            img, robotview, t1 = vision.StartCapturing()
+            img, robotview, t1 = vision.Capturing()
             
             contoursItem, ItemMask = vision.findItems(img)
             contoursShelf, ShelfMask = vision.findShelf(img)
             contoursLoadingArea, LoadingAreaMask = vision.findLoadingArea(img)
+
             Mask = ItemMask | ShelfMask
             robotview, x1, y1, x2, y2 = vision.DrawContours(contoursItem, robotview, (0, 255, 0), "Item")
             robotview, x1, y1, x2, y2 = vision.DrawContours(contoursShelf, robotview, (0, 0, 255), "Shelf")
             result = cv2.bitwise_and(robotview, robotview, mask=Mask)
 
-            calculate_and_display_fps(robotview, t1)
-
-            cv2.imshow("RobotView", robotview)     # Display the obtained frame in a window called "CameraImage"
+            #vision.ExportImage("Masking", result, FPS = False)
+            vision.ExportImage("RobotView", robotview, FPS = True)
             if cv2.waitKey(1) & 0xFF == ord('q'): # Press 'q' to quit
                   break
                   
       cap.close()
 
 if __name__ == "__main__": # Run the main function
-      CSV = CSVReader('Order_1.csv')
+      CSV = csvread.CSVReader('Order_1.csv')
       CSV.read_csv()
       instruction = CSV.RobotInstruction() # Generating robot instructions and print instructions
       main()
