0164ca0581e33b3d8bc00c0c5e628c1280317869
Nizdclxvikit <jasper.yapser@gmail.com>
2024-09-02 16:13:58 +1000

    Outline each state as a class
    readable nice hopefully

--- COPPELIA_FILES/PythonCode/simMain_simple.py
+++ COPPELIA_FILES/PythonCode/simMain_simple.py
@@ -22,73 +22,106 @@ STATE_DESCEND_PACKING = 11
 PHASE_COLLECT = 0
 PHASE_DROPOFF = 1
 
+class State:
+	
+	def start(self):
+		pass
+
+	def update(self):
+		pass
+
+def State_Wander(State):
+	def __init__(self, packerBotSim):
+		self.pbs = packerBotSim
+		self.found_aisle = False
+		self.wander_bias = 0
+	
+	def start(self):
+		self.found_aisle = False
+		self.wander_bias = 0
+
+	def update(self):
+		self.pbs.SetTargetVelocities()
+		return STATE_WANDER
+
+
 class RobotStates:
 	def __init__(self):
 		self.current_state = STATE_LOST
 		self.current_phase = PHASE_COLLECT
+		self.state_o = State_Wander(packerBotSim)
 
-		self.item_location = (0,0,0) #shelf, bay, level (or something)
+		
 
 	def update(self, packerBotSim, itemRangeBearing, packingBayRangeBearing, obstaclesRangeBearing, rowMarkerRangeBearing, shelfRangeBearing):
-		match self.current_state:
-			case 0: # STATE_LOST
-				# Print error message
-				# -> STATE_WANDER
-				pass
-			case 1: # STATE_WANDER
-				# Just wander around until a marker is found
-				# Avoid obstacles
-				# -> STATE_AISLE_DOWN
-				pass
-			case 2: # STATE_AISLE_DOWN
-				# Go down the aisle to the right bay
-				# Avoid obstacles
-				# -> STATE_FACE_BAY
-				pass
-			case 3: # STATE_FACE_BAY
-				# Turn to the bay and position in front of it
-				# -> STATE_COLLECT_ITEM
-				pass
-			case 4: # STATE_COLLECT_ITEM
-				# Reach up ad grab the item
-				# -> STATE_FACE_OUT
-				pass
-			case 5: # STATE_FACE_OUT
-				# Turn to face out of the aisle (need to remember which side we're on)
-				# -> STATE_WANDER_OUT
-				pass
-			case 6: # STATE_WANDER_OUT
-				# Go forward out of the aisle until we see the pakcing station.
-				# If we hit a wall turn right
-				# Don't enter an aisle
-				# Avoid obstacles
-				# -> STATE_FACE_PACKING
-				pass
-			case 7: # STATE_FACE_PACKING
-				# Turn to face packing station
-				# -> STATE_APPROACH_PACKING
-				pass
-			case 8: # STATE_APPROACH_PACKING
-				# Go to the packing station but don't do up it
-				# Avoid obstacles
-				# STATE_ASCEND_PACKING
-				pass
-			case 9: # STATE_ASCEND_PACKING
-				# Go slowly up the packing station
-				# -> STATE_DROP_ITEM
-				pass
-			case 10: # STATE_DROP_ITEM
-				# (Lower the item)
-				# Drop the item
-				# -> STATE_DESCEND_PACKING
-				pass
-			case 11: # STATE_DESCEND_PACKING
-				# Go backwards slowly down the packing station
-				# Then turn 180 degrees
-				# -> STATE_WANDER
-				pass
+		new_state = self.state_o.update()
+		if new_state != self.current_state:
+			self.current_state = new_state
+			self.state_o = State_Wander(packerBotSim)
+			self.state_o.start()
+		
 
-		packerBotSim.SetTargetVelocities(0,0)
+
+	# def update(self, packerBotSim, itemRangeBearing, packingBayRangeBearing, obstaclesRangeBearing, rowMarkerRangeBearing, shelfRangeBearing):
+	# 	match self.current_state:
+	# 		case 0: # STATE_LOST
+	# 			# Print error message
+	# 			# -> STATE_WANDER
+	# 			pass
+	# 		case 1: # STATE_WANDER
+	# 			# Just wander around until a marker is found
+	# 			# Avoid obstacles
+	# 			# -> STATE_AISLE_DOWN
+	# 			pass
+	# 		case 2: # STATE_AISLE_DOWN
+	# 			# Go down the aisle to the right bay
+	# 			# Avoid obstacles
+	# 			# -> STATE_FACE_BAY
+	# 			pass
+	# 		case 3: # STATE_FACE_BAY
+	# 			# Turn to the bay and position in front of it
+	# 			# -> STATE_COLLECT_ITEM
+	# 			pass
+	# 		case 4: # STATE_COLLECT_ITEM
+	# 			# Reach up ad grab the item
+	# 			# -> STATE_FACE_OUT
+	# 			pass
+	# 		case 5: # STATE_FACE_OUT
+	# 			# Turn to face out of the aisle (need to remember which side we're on)
+	# 			# -> STATE_WANDER_OUT
+	# 			pass
+	# 		case 6: # STATE_WANDER_OUT
+	# 			# Go forward out of the aisle until we see the pakcing station.
+	# 			# If we hit a wall turn right
+	# 			# Don't enter an aisle
+	# 			# Avoid obstacles
+	# 			# -> STATE_FACE_PACKING
+	# 			pass
+	# 		case 7: # STATE_FACE_PACKING
+	# 			# Turn to face packing station
+	# 			# -> STATE_APPROACH_PACKING
+	# 			pass
+	# 		case 8: # STATE_APPROACH_PACKING
+	# 			# Go to the packing station but don't do up it
+	# 			# Avoid obstacles
+	# 			# STATE_ASCEND_PACKING
+	# 			pass
+	# 		case 9: # STATE_ASCEND_PACKING
+	# 			# Go slowly up the packing station
+	# 			# -> STATE_DROP_ITEM
+	# 			pass
+	# 		case 10: # STATE_DROP_ITEM
+	# 			# (Lower the item)
+	# 			# Drop the item
+	# 			# -> STATE_DESCEND_PACKING
+	# 			pass
+	# 		case 11: # STATE_DESCEND_PACKING
+	# 			# Go backwards slowly down the packing station
+	# 			# Then turn 180 degrees
+	# 			# -> STATE_WANDER
+	# 			pass
+
+	# 	packerBotSim.SetTargetVelocities(0,0)
 
 			
 	
