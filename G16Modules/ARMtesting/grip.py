0b896a1e5d748c288a677db20941fb7d51f88a92
kelvin <nguyenleminh1002@gmail.com>
2024-09-16 10:29:22 +1000

    Add item collection code

---
+++ G16Modules/ARMtesting/grip.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+p = GPIO.PWM(12,50)
+
+print("starting")
+p.start(6.85)
+
+def close (seconds):
+    p.ChangeDutyCycle(7)
+    time.sleep(seconds)
+def open (seconds):
+    p.ChangeDutyCycle(6.7)
+    time.sleep(seconds)
+
+def hold (seconds):
+    p.ChangeDutyCycle(7.4)
+    time.sleep(seconds)
+
+def stop():#greater num = close less num = open
+    p.ChangeDutyCycle(6.95)
+
+
+open(1)
+close(1)
+hold(2)
+open(1)
+stop()
+time.sleep(5)
+
+open(1)
+close(1)
+hold(2)
+open(1)
+stop()
+time.sleep(5)
+
+open(1)
+close(1)
+hold(2)
+open(1)
+stop()
+time.sleep(5)
